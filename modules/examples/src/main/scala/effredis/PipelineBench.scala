4cfbb3fc2dc2a9dc73fb6c139fd3e862dbfde851
Debasish Ghosh <ghosh.debasish@gmail.com>
2020-08-13 21:05:34 +0530

    Added a bench for pipeline

--- modules/examples/src/main/scala/effredis/PipelineBench.scala
+++ modules/examples/src/main/scala/effredis/PipelineBench.scala
@@ -20,50 +20,38 @@ import cats.effect._
 import cats.implicits._
 import log4cats._
 
-object PBench extends LoggerIOApp {
-  def setter(keyPrefix: String, valPrefix: String) =
-    RedisClient.make[IO](new java.net.URI("http://localhost:6379")).use { cmd =>
-      val nKeys = 12500
-      (0 to nKeys)
-        .map { i =>
-          for {
-            a <- cmd.set(s"$keyPrefix$i", s"$valPrefix $i")
-          } yield a
+object PipelineBench extends LoggerIOApp {
+
+  def setupPipeline(keyPrefix: String, valPrefix: String): IO[Resp[Option[List[Any]]]] = {  
+    RedisClient.make[IO](new java.net.URI("http://localhost:6379")).use { cli =>
+      RedisClient.withSequencingDecorator[IO](cli, true).use { txnClient =>
+        import txnClient._
+
+        val nKeys = 12500
+        parent.pipeline(txnClient) { () =>
+          (0 to nKeys).map { i =>
+            set(s"$keyPrefix$i", s"$valPrefix $i")
+          }
         }
-        .toList
-        .sequence
+      }
     }
+  }
 
   override def run(args: List[String]): IO[ExitCode] = {
 
     val s = System.currentTimeMillis()
 
-    val res = for {
-      s1 <- setter("key-1", "value-1").start
-      s2 <- setter("key-2", "value-2").start
-      s3 <- setter("key-3", "value-3").start
-      s4 <- setter("key-4", "value-4").start
-      s5 <- setter("key-5", "value-5").start
-      s6 <- setter("key-6", "value-6").start
-      s7 <- setter("key-7", "value-7").start
-      s8 <- setter("key-8", "value-8").start
-      j1 <- s1.join
-      j2 <- s2.join
-      j3 <- s3.join
-      j4 <- s4.join
-      j5 <- s5.join
-      j6 <- s6.join
-      j7 <- s7.join
-      j8 <- s8.join
-    } yield List(j1, j2, j3, j4, j5, j6, j7, j8)
+    val res = (0 to 7).map { i =>
+      setupPipeline(s"key-$i", s"value-$i")
+    }.toList.sequence
     res.unsafeRunSync()
 
     val timeElapsedSet = (System.currentTimeMillis() - s) / 1000
     println(s"Time elapsed in setting 100000 keys = $timeElapsedSet seconds")
     println(s"Rate = ${100000 / timeElapsedSet} sets per second")
 
-    // Time elapsed in setting 100000 keys = 2 seconds
-    // Rate = 500000 sets per second
+    // Time elapsed in setting 100000 keys = 1 seconds
+    // Rate = 100000 sets per second
 
     IO(ExitCode.Success)
   }
