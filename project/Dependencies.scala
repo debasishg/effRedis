7f4459b1d99456ceba32094338f99f10bb470b88
Scala Steward <me@scala-steward.org>
2020-11-01 16:17:08 +0100

    Update scalacheck-effect-munit to 0.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     val cormorant = "0.3.0"
     val keypool = "0.2.0"
     val kittens = "2.2.0"
-    val munitScalacheckEffect = "0.2.0"
+    val munitScalacheckEffect = "0.3.0"
     val munitCatsEffect = "0.3.0"
   }
 
