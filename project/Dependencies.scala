d6a16b25cc3990f6f41f3452c7a94f3190bf44ae
Scala Steward <me@scala-steward.org>
2021-11-11 20:25:31 +0100

    Update logback-classic to 1.2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,18 +13,18 @@ object Dependencies {
     val log4cats   = "1.1.1"
     val shapeless  = "2.4.0-M1"
 
-    val logback = "1.2.6"
+    val logback = "1.2.7"
 
     val betterMonadicFor = "0.3.1"
     val contextApplied   = "0.1.4"
     val kindProjector    = "0.13.2"
 
-    val munit = "0.7.29"
-    val enumeratum = "1.7.0"
-    val cormorant = "0.3.0"
-    val keypool = "0.2.0"
-    val kittens = "2.3.2"
-    val munitCatsEffect = "1.0.6"
+    val munit                 = "0.7.29"
+    val enumeratum            = "1.7.0"
+    val cormorant             = "0.3.0"
+    val keypool               = "0.2.0"
+    val kittens               = "2.3.2"
+    val munitCatsEffect       = "1.0.6"
     val munitScalacheckEffect = "1.0.3"
   }
 
@@ -32,23 +32,22 @@ object Dependencies {
     def cats(artifact: String): ModuleID     = "org.typelevel"     %% s"cats-$artifact"     % V.cats
     def log4cats(artifact: String): ModuleID = "io.chrisdavenport" %% s"log4cats-$artifact" % V.log4cats
 
-    val catsEffect = "org.typelevel"     %% "cats-effect"        % V.catsEffect
-    val fs2Core    = "co.fs2"            %% "fs2-core"           % V.fs2
-    val shapeless  = "com.chuusai"       %% "shapeless"          % V.shapeless
-    val enumeratum = "com.beachape"      %% "enumeratum"         % V.enumeratum
-    val keypool    = "io.chrisdavenport" %% "keypool"            % V.keypool
-    val kittens    = "org.typelevel"     %% "kittens"            % V.kittens
-
+    val catsEffect = "org.typelevel"     %% "cats-effect" % V.catsEffect
+    val fs2Core    = "co.fs2"            %% "fs2-core"    % V.fs2
+    val shapeless  = "com.chuusai"       %% "shapeless"   % V.shapeless
+    val enumeratum = "com.beachape"      %% "enumeratum"  % V.enumeratum
+    val keypool    = "io.chrisdavenport" %% "keypool"     % V.keypool
+    val kittens    = "org.typelevel"     %% "kittens"     % V.kittens
 
     object Cormorant {
-      val core              = "io.chrisdavenport" %% "cormorant-core"     % V.cormorant
-      val generic           = "io.chrisdavenport" %% "cormorant-generic"  % V.cormorant
-      val parser            = "io.chrisdavenport" %% "cormorant-parser"   % V.cormorant
+      val core    = "io.chrisdavenport" %% "cormorant-core"    % V.cormorant
+      val generic = "io.chrisdavenport" %% "cormorant-generic" % V.cormorant
+      val parser  = "io.chrisdavenport" %% "cormorant-parser"  % V.cormorant
     }
 
-    val slf4jApi   = "org.slf4j"      %  "slf4j-api"         % "1.7.32"
-    val slf4jLog4j = "org.slf4j"      %  "slf4j-log4j12"     % "1.7.32"      % "provided"
-    val log4j      = "log4j"          %  "log4j"             % "1.2.17"      % "provided"
+    val slf4jApi   = "org.slf4j" % "slf4j-api"     % "1.7.32"
+    val slf4jLog4j = "org.slf4j" % "slf4j-log4j12" % "1.7.32" % "provided"
+    val log4j      = "log4j"     % "log4j"         % "1.2.17" % "provided"
 
     val log4CatsCore = log4cats("core")
 
@@ -60,11 +59,11 @@ object Dependencies {
     val logback       = "ch.qos.logback" % "logback-classic" % V.logback
 
     // Testing libraries
-    val catsLaws         = cats("core")
-    val catsTestKit      = cats("testkit")
-    val munitCore        = "org.scalameta" %% "munit" % V.munit
+    val catsLaws              = cats("core")
+    val catsTestKit           = cats("testkit")
+    val munitCore             = "org.scalameta" %% "munit" % V.munit
     val munitScalacheckEffect = "org.typelevel" %% "scalacheck-effect-munit" % V.munitScalacheckEffect
-    val munitCatsEffect = "org.typelevel" %% "munit-cats-effect-2" % V.munitCatsEffect
+    val munitCatsEffect       = "org.typelevel" %% "munit-cats-effect-2" % V.munitCatsEffect
   }
 
   object CompilerPlugins {
